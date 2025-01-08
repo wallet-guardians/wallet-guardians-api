82a2f82f92b794edb81c8abbd7e64a9eb566413f
나경호 <hoyana1225@gmail.com>
2025-01-08 21:49:29 +0900

    init: start project 🎉

---
+++ src/test/java/com/walletguardians/walletguardiansapi/WalletGuardiansApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletguardians.walletguardiansapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletGuardiansApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
