be5aa808071c8d3951a9a6c1d5dfc265dcde3fe5
ahyeungii <ahyeungii@gmail.com>
2025-01-14 14:14:38 +0900

    feat: add ExpenseRepository

---
+++ src/main/java/com/walletguardians/walletguardiansapi/expenses/repository/ExpenseRepository.java
@@ -0,0 +1,11 @@
+package com.walletguardians.walletguardiansapi.expenses.repository;
+
+import com.walletguardians.walletguardiansapi.expenses.entity.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
