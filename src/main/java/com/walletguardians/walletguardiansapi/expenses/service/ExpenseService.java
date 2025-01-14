01c856d2756f24cb5195ac7e70921aaf55c3ab60
ahyeungii <ahyeungii@gmail.com>
2025-01-14 15:35:23 +0900

    feat: get all expenses

--- src/main/java/com/walletguardians/walletguardiansapi/expenses/service/ExpenseService.java
+++ src/main/java/com/walletguardians/walletguardiansapi/expenses/service/ExpenseService.java
@@ -1,13 +1,16 @@
 package com.walletguardians.walletguardiansapi.expenses.service;
 
 import com.walletguardians.walletguardiansapi.expenses.controller.dto.request.CreateExpenseRequest;
+import com.walletguardians.walletguardiansapi.expenses.controller.dto.response.ExpenseResponse;
 import com.walletguardians.walletguardiansapi.expenses.repository.ExpenseRepository;
 import com.walletguardians.walletguardiansapi.expenses.entity.Expense;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,4 +24,14 @@ public class ExpenseService {
         expenseRepository.save(expense);
     }
 
+    public List<ExpenseResponse> getExpenses(Date date) {
+        List<Expense> expenses = expenseRepository.findAll();
+        List<ExpenseResponse> expenseResponses = new ArrayList<>();
+        for (Expense expense : expenses) {
+            if (expense.getDate().equals(date)) {
+                expenseResponses.add(ExpenseResponse.from(expense));
+            }
+        }
+        return expenseResponses;
+    }
 }
