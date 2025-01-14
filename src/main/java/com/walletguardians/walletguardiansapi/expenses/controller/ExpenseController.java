ae35ec05ae0c7805aff4caf415eef80d445d07cc
ahyeungii <ahyeungii@gmail.com>
2025-01-14 14:15:00 +0900

    feat: add ExpenseController

---
+++ src/main/java/com/walletguardians/walletguardiansapi/expenses/controller/ExpenseController.java
@@ -0,0 +1,21 @@
+package com.walletguardians.walletguardiansapi.expenses.controller;
+
+import com.walletguardians.walletguardiansapi.expenses.controller.dto.request.CreateExpenseRequest;
+import com.walletguardians.walletguardiansapi.expenses.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/expense")
+@RequiredArgsConstructor
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    @PostMapping("/{date}")
+    public void createExpense(@RequestBody CreateExpenseRequest createExpenseRequest, @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        expenseService.createExpense(date, createExpenseRequest);
+    }
+}
