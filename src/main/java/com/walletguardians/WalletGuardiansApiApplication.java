2e53abe4614d02e986c713f0e1eef22a266cd27f
enohs <kmsn01@naver.com>
2025-01-18 13:25:33 +0900

    move: 파일 위치 변경

--- src/main/java/com/walletguardians/WalletGuardiansApiApplication.java
+++ src/main/java/com/walletguardians/WalletGuardiansApiApplication.java
@@ -1,4 +1,4 @@
-package com.walletguardians.walletguardiansapi;
+package com.walletguardians;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
