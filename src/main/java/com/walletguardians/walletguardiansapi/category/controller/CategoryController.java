368285610201e26adf2c13155b882d478c3e2c5e
ahyeungii <ahyeungii@gmail.com>
2025-01-20 03:16:14 +0900

    feat: add category controller

---
+++ src/main/java/com/walletguardians/walletguardiansapi/category/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package com.walletguardians.walletguardiansapi.category.controller;
+
+import com.walletguardians.walletguardiansapi.category.entity.Category;
+import com.walletguardians.walletguardiansapi.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    // 클라이언트에게 카테고리에 대한 정보를 주기 위한
+    // 카테고리 조회 API
+    @GetMapping()
+    public List<Category> getCategories() {
+        List<Category> categories = new ArrayList<>();
+        return categoryService.getCategories();
+    }
+
+}
