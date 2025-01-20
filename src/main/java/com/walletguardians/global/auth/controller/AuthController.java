8deddfe60744852f3ef8e86b06351cff3119673a
enohs <kmsn01@naver.com>
2025-01-20 16:07:46 +0900

    style: 매핑 주소 변경

--- src/main/java/com/walletguardians/global/auth/controller/AuthController.java
+++ src/main/java/com/walletguardians/global/auth/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auths")
+@RequestMapping("/api/auth")
 public class AuthController {
 
   private final AuthService authService;
