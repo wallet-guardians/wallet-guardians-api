620ba188b95978e9d1ab3a19aada57775ee4f0a1
ahyeungii <ahyeungii@gmail.com>
2025-01-20 15:28:45 +0900

    feat: Association mapping between Expense and Category
    Expense 엔티티와 Category 엔티티 사이의 다대일 연관관계를 매핑했습니다.

--- src/main/java/com/walletguardians/walletguardiansapi/category/entity/Category.java
+++ src/main/java/com/walletguardians/walletguardiansapi/category/entity/Category.java
@@ -1,10 +1,14 @@
 package com.walletguardians.walletguardiansapi.category.entity;
 
+import com.walletguardians.walletguardiansapi.expenses.entity.Expense;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,6 +20,9 @@ public class Category {
     private long id;
 
     private String categoryName;
+
+    @OneToMany(mappedBy = "category") // 나중에 쓸지도 모르니 양방향 매핑
+    private List<Expense> expenses = new ArrayList<>();
 
     @Builder
     public Category(Long id, String categoryName) {
