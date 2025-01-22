f2cd1f07d9b9c5e4e29428f804979502a3d8c9df
ahyeungii <ahyeungii@gmail.com>
2025-01-22 23:43:43 +0900

    remove: remove Category API
    카테고리를 table로 관리하지 않고 enum으로 관리하기로 해서 삭제했습니다.

--- src/main/java/com/walletguardians/walletguardiansapi/domain/category/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.walletguardians.walletguardiansapi.domain.category.repository;
-
-import com.walletguardians.walletguardiansapi.domain.category.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
