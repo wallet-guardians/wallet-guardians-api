95d42a028ebf93f203515829c1fcb552f407f65f
Jungsukwoo <jungsw1106@naver.com>
2025-01-24 16:27:29 +0900

    refactor: API Response Type 통일

--- src/main/java/com/walletguardians/walletguardiansapi/global/auth/controller/AuthController.java
+++ src/main/java/com/walletguardians/walletguardiansapi/global/auth/controller/AuthController.java
@@ -2,15 +2,12 @@ package com.walletguardians.walletguardiansapi.global.auth.controller;
 
 import com.walletguardians.walletguardiansapi.domain.user.dto.request.UserLoginRegister;
 import com.walletguardians.walletguardiansapi.domain.user.dto.request.UserRegisterRequest;
-import com.walletguardians.walletguardiansapi.global.auth.CustomUserDetails;
 import com.walletguardians.walletguardiansapi.global.auth.jwt.dto.TokenDto;
-import com.walletguardians.walletguardiansapi.global.auth.jwt.service.JwtService;
 import com.walletguardians.walletguardiansapi.global.auth.service.AuthService;
-import jakarta.servlet.http.HttpServletRequest;
+import com.walletguardians.walletguardiansapi.global.response.BaseResponse;
+import com.walletguardians.walletguardiansapi.global.response.BaseResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,27 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-  private final JwtService jwtService;
   private final AuthService authService;
+  private final BaseResponseService baseResponseService;
 
-  @PostMapping("/signup")
-  public ResponseEntity<String> signUp(@RequestBody UserRegisterRequest userRegisterRequest) {
-    authService.signUpUser(userRegisterRequest);
+  @PostMapping("/sign-up")
+  public ResponseEntity<String> register(@RequestBody UserRegisterRequest userRegisterRequest) {
+    authService.registerUser(userRegisterRequest);
     return ResponseEntity.ok().body("성공적으로 회원등록이 완료되었습니다.");
   }
 
   @PostMapping("/login")
-  public ResponseEntity<TokenDto> login(@RequestBody UserLoginRegister userLoginRegister) {
-    return ResponseEntity.ok().body(authService.login(userLoginRegister));
+  public ResponseEntity<BaseResponse<TokenDto>> login(@RequestBody UserLoginRegister userLoginRegister) {
+    TokenDto tokenDto = authService.login(userLoginRegister);
+    return ResponseEntity.ok(baseResponseService.getSuccessResponse(tokenDto));
   }
-
-  @DeleteMapping("/logout")
-  public ResponseEntity<String> logout(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-      HttpServletRequest request) {
-    String accessToken = jwtService.extractAccessToken(request)
-        .filter(jwtService::validateToken)
-        .orElse(null);
-    return authService.logout(accessToken, customUserDetails.getUsername()); //username = email
-  }
-
 }
---
+++ src/main/java/com/walletguardians/walletguardiansapi/global/response/BaseResponseStatus.java
@@ -0,0 +1,41 @@
+package com.walletguardians.walletguardiansapi.global.response;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+
+    SUCCESS(true, 2000, "요청에 성공했습니다."),
+
+    NOT_FOUND_MEMBER_ID(false, 2001, "해당하는 회원 정보가 없습니다."),
+
+    INVALID_PASSWORD(false, 2002, "해당하는 비밀번호가 없습니다."),
+
+    INVALID_EMAIL(false, 2003, "해당하는 이메일 정보가 없습니다."),
+
+    /**
+    * Calendar
+    * Code : 4000번대
+    */
+     NOT_FOUND_CALENDAR_INFO(false, 4001, "캘린더가 존재하지 않습니다.");
+
+
+    // -------- 실패 코드 종료 -------- //
+
+    private boolean isSuccess; // 성공 여부
+    private String message; // 메시지
+    private int code; // 코드
+
+    /**
+     * BaseResponseStatus 에서 해당하는 코드를 매핑
+     *
+     * @param isSuccess
+     * @param code
+     * @param message
+     */
+    BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
