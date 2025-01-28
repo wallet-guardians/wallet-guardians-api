f6e244af36c691b72fcef3768b234052923c9cda
ahyeungii <ahyeungii@gmail.com>
2025-01-29 03:14:07 +0900

    rename: rename CreateReceiptDTO
    요청에 사용되는 dto이므로 Request가 더 적절한 것 같아 이름을 수정했습니다.
    
    영수증 엔티티가 있을 필요가 없어 삭제했습니다.

--- src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/controller/dto/request/CreateReceiptRequest.java
+++ src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/controller/dto/request/CreateReceiptRequest.java
@@ -1,12 +1,10 @@
 package com.walletguardians.walletguardiansapi.domain.expenses.controller.dto.request;
-import com.walletguardians.walletguardiansapi.domain.expenses.entity.Expense;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 
 @Getter
-public class CreateReceiptDTO implements Serializable {
+public class CreateReceiptRequest implements Serializable {
     private String category;
 
     private String description;;
