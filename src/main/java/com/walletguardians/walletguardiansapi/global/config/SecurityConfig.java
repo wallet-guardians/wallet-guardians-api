83ee5b371c0bf44d5b94297f2a433fcda43ea4c1
enohs <kmsn01@naver.com>
2025-02-02 01:23:27 +0900

    style: 프론트 배포 url 추가

--- src/main/java/com/walletguardians/walletguardiansapi/global/config/SecurityConfig.java
+++ src/main/java/com/walletguardians/walletguardiansapi/global/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+    configuration.setAllowedOrigins(
+        Arrays.asList("http://localhost:5173", "https://wallet-guardians-web.vercel.app"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setMaxAge(3600L);
