308d6ed7c5ffe6c6a6e2a0649736b587aa60a10f
enohs <kmsn01@naver.com>
2025-02-01 19:53:19 +0900

    style: 로그아웃 리턴값 수정

--- src/main/java/com/walletguardians/walletguardiansapi/global/auth/controller/AuthController.java
+++ src/main/java/com/walletguardians/walletguardiansapi/global/auth/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.walletguardians.walletguardiansapi.global.auth.controller;
 
-import com.walletguardians.walletguardiansapi.domain.user.controller.dto.request.UserLoginRequest;
-import com.walletguardians.walletguardiansapi.domain.user.controller.dto.request.UserRegisterRequest;
-
+import com.walletguardians.walletguardiansapi.domain.user.dto.request.UserLoginRegister;
+import com.walletguardians.walletguardiansapi.domain.user.dto.request.UserRegisterRequest;
+import com.walletguardians.walletguardiansapi.global.auth.CustomUserDetails;
 import com.walletguardians.walletguardiansapi.global.auth.jwt.dto.TokenDto;
+import com.walletguardians.walletguardiansapi.global.auth.jwt.service.JwtService;
 import com.walletguardians.walletguardiansapi.global.auth.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import com.walletguardians.walletguardiansapi.global.response.BaseResponse;
 import com.walletguardians.walletguardiansapi.global.response.BaseResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+  private final JwtService jwtService;
   private final AuthService authService;
   private final BaseResponseService baseResponseService;
 
@@ -35,5 +39,14 @@ public class AuthController {
     return ResponseEntity.ok(baseResponseService.getSuccessResponse(tokenDto));
   }
 
+  @DeleteMapping("/logout")
+  public ResponseEntity<BaseResponse<Void>> logout(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+      HttpServletRequest request) {
+    String accessToken = jwtService.extractAccessToken(request)
+        .filter(jwtService::validateToken)
+        .orElse(null);
+    authService.logout(accessToken, customUserDetails.getUsername()); //username = email
+    return ResponseEntity.ok().body(baseResponseService.getSuccessResponse());
+  }
 }
 
