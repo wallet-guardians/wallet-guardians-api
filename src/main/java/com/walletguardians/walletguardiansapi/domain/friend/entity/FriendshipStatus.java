a71496f4bee12307dcbb353420f2368513b4cc7e
Jungsukwoo <jungsw1106@naver.com>
2025-02-04 19:16:00 +0900

    feat: 친구추가 요청 거절, 친구 삭제 기능 추가

--- src/main/java/com/walletguardians/walletguardiansapi/domain/friend/entity/FriendshipStatus.java
+++ src/main/java/com/walletguardians/walletguardiansapi/domain/friend/entity/FriendshipStatus.java
@@ -26,17 +26,18 @@ public class FriendshipStatus {
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "friendship_status", nullable = false)
-    private boolean friendshipStatus;
+    private FriendshipStatusEnum friendshipStatus;
 
     @Builder
-    public FriendshipStatus(User sender, User receiver, boolean friendshipStatus) {
+    public FriendshipStatus(User sender, User receiver, FriendshipStatusEnum friendshipStatus) {
         this.sender = sender;
         this.receiver = receiver;
         this.friendshipStatus = friendshipStatus;
     }
 
-    public void updateFriendshipStatus(boolean status) {
+    public void updateFriendshipStatus(FriendshipStatusEnum status) {
         this.friendshipStatus = status;
     }
 }
