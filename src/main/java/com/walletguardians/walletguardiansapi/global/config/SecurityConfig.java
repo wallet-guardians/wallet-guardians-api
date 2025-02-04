4264c7939da5f8e638a8c2d5e61f5a0b827d1c4d
enohs <kmsn01@naver.com>
2025-02-04 21:20:13 +0900

    style: 프론트 주소 변경

--- src/main/java/com/walletguardians/walletguardiansapi/global/config/SecurityConfig.java
+++ src/main/java/com/walletguardians/walletguardiansapi/global/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(
-        Arrays.asList("http://localhost:5173", "https://wallet-guardians-web.vercel.app"));
+        Arrays.asList("http://localhost:5173", "https://wallet-guardians.vercel.app"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setMaxAge(3600L);
