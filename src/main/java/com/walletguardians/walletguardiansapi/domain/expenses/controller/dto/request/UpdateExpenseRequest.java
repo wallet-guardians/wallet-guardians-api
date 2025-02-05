ea52f7908b152be0b3afc4d25c92e27213ecb618
enohs <kmsn01@naver.com>
2025-02-06 05:32:50 +0900

    style: Getter 추가

--- src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/controller/dto/request/UpdateExpenseRequest.java
+++ src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/controller/dto/request/UpdateExpenseRequest.java
@@ -1,20 +1,22 @@
 package com.walletguardians.walletguardiansapi.domain.expenses.controller.dto.request;
 
 import com.walletguardians.walletguardiansapi.domain.expenses.entity.Expense;
+import lombok.Getter;
 
+@Getter
 public class UpdateExpenseRequest {
 
-    private String category;
-    private int amount;
-    private String storeName;
-    private String description;
+  private String category;
+  private int amount;
+  private String storeName;
+  private String description;
 
-    public Expense toEntity() {
-        return Expense.builder()
-                .category(category)
-                .amount(amount)
-                .storeName(storeName)
-                .description(description)
-                .build();
-    }
+  public Expense toEntity() {
+    return Expense.builder()
+        .category(category)
+        .amount(amount)
+        .storeName(storeName)
+        .description(description)
+        .build();
+  }
 }
