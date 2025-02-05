fd47e357c7116fa0990a0cb534d8853fbbacfbc7
enohs <kmsn01@naver.com>
2025-02-06 05:36:18 +0900

    style: 엔티티 변경

--- src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/entity/Expense.java
+++ src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/entity/Expense.java
@@ -1,15 +1,16 @@
 package com.walletguardians.walletguardiansapi.domain.expenses.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.walletguardians.walletguardiansapi.domain.user.entity.User;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Expense {
 
@@ -21,12 +22,10 @@ public class Expense {
     @Column(nullable = false)
     private int amount;
 
-    @Column // null일 수 있음
     private String description;
 
     @Column(nullable = false)
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date date;
+    private LocalDateTime date;
 
     @Column(nullable = false)
     private String category;
@@ -34,23 +33,10 @@ public class Expense {
     @Column(nullable = false, name = "store_name")
     private String storeName;
 
-    @Column(name = "img_path") // null일 수 있음
-    private String imagePath;
-
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user; // User 엔티티와의 관계
-
-    @Builder
-    private Expense(Long id, int amount, String description, Date date, String category, String storeName, String imagePath) {
-        this.id = id;
-        this.amount = amount;
-        this.description = description;
-        this.date = date;
-        this.category = category;
-        this.storeName = storeName;
-        this.imagePath = imagePath;
-    }
+    private User user;
 
     public void update(Expense updateExpense) {
         this.category = updateExpense.category;
