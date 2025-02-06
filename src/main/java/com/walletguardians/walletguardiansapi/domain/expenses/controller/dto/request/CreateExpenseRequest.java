7356365b01128387cb31d975ba10ad0e5c4d3950
enohs <kmsn01@naver.com>
2025-02-06 18:16:28 +0900

    style: toEntity 변경

--- src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/controller/dto/request/CreateExpenseRequest.java
+++ src/main/java/com/walletguardians/walletguardiansapi/domain/expenses/controller/dto/request/CreateExpenseRequest.java
@@ -3,6 +3,7 @@ package com.walletguardians.walletguardiansapi.domain.expenses.controller.dto.re
 import com.walletguardians.walletguardiansapi.domain.expenses.entity.Expense;
 
 import com.walletguardians.walletguardiansapi.domain.user.entity.User;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,14 +19,13 @@ public class CreateExpenseRequest {
 
   private String storeName;
 
-  private User user;
-
-  public Expense toEntity() {
+  public Expense toEntity(User user, LocalDate date) {
     return Expense.builder()
         .amount(amount)
         .category(category)
         .description(description)
         .storeName(storeName)
+        .date(date)
         .user(user)
         .build();
   }
