ae74fc2d3f2cecd675b7977538b9532d7ece5e5c
Jungsukwoo <jungsw1106@naver.com>
2025-02-09 00:13:08 +0900

    feat/회원정보 수정 DTO 생성

---
+++ src/main/java/com/walletguardians/walletguardiansapi/domain/user/dto/request/UserUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.walletguardians.walletguardiansapi.domain.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserUpdateRequest {
+
+    private String username;
+    private String title;
+}
