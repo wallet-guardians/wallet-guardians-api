25d9646278b2b24b18a438e51a257ab022da8c03
enohs <kmsn01@naver.com>
2025-02-13 00:24:22 +0900

    fix: 테스트 시 발생한 문제 수정 및 파일 위치 변경

--- src/main/java/com/walletguardians/walletguardiansapi/global/security/CustomUserDetails.java
+++ src/main/java/com/walletguardians/walletguardiansapi/global/security/CustomUserDetails.java
@@ -25,7 +25,7 @@ public class CustomUserDetails implements UserDetails {
   }
 
   public Long getUserId() {
-    return user.getId(); // User 객체에서 userId를 반환
+    return user.getId();
   }
 
   @Override
